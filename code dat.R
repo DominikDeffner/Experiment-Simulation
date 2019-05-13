88f39dffa921fcf0dd92b4fa10ba83d818adf167
DominikDeffner <33870194+DominikDeffner@users.noreply.github.com>
2019-05-13 11:00:05 +0200

    Add files via upload

---
+++ code dat.R
@@ -0,0 +1,29 @@
+# code data
+
+dat <- as.list(Result)
+dat$farm <- as.integer( ceiling( dat$trial / 25 ) )
+dat$new_farm <- sapply( 1:nrow(Result) , function(i) {
+	if ( i>1 ) {
+		return( ifelse(dat$farm[i]!=dat$farm[i-1],1,0) )
+	} else {
+		return(0)
+	}
+} )
+
+dat$group_id <- dat$group.id
+dat$group.id <- NULL
+
+# prep id's
+dat$uid <- (dat$Session_ID - 1)*8 + dat$id
+dat$N_id <- max(dat$uid)
+dat$N <- nrow(Result)
+
+nmat <- cbind( dat$n1 , dat$n2 , dat$n3 , dat$n4 )
+
+dat$nmat <- nmat
+
+library(rethinking)
+
+m0 <- stan( file="ewa_model1.stan" , data=dat , chains=1 )
+
+m1 <- stan( file="ewa_model2.stan" , data=dat , chains=1 )
