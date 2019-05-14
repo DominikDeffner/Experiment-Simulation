ffb51b5f521f6fa64c750ea9da6046e51220439f
DominikDeffner <33870194+DominikDeffner@users.noreply.github.com>
2019-05-14 17:01:35 +0200

    Add files via upload

--- code dat.R
+++ code dat.R
@@ -19,11 +19,20 @@ dat$N_id <- max(dat$uid)
 dat$N <- nrow(Result)
 
 nmat <- cbind( dat$n1 , dat$n2 , dat$n3 , dat$n4 )
+choice_models <- cbind(dat$Choice1, dat$Choice2, dat$Choice3)
+age_models    <- cbind(dat$Age1, dat$Age2, dat$Age3)
+
+choice_models[is.na(choice_models)] <- 0
+age_models[is.na(age_models)] <- 0
 
 dat$nmat <- nmat
+dat$choice_models <- choice_models
+dat$age_models <- age_models
 
 library(rethinking)
 
 m0 <- stan( file="ewa_model1.stan" , data=dat , chains=1 )
 
 m1 <- stan( file="ewa_model2.stan" , data=dat , chains=1 )
+
+m2 <- stan( file="ewa_model3.stan" , data=dat , chains=1 )
